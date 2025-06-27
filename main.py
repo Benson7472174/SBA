c4e5dfbc3ea59ad42e8f146f38750d62f83d2956
Benson7472174 <98572896+Benson7472174@users.noreply.github.com>
2025-06-27 13:27:55 +0800

    Release 0.4 Add create account function Fix login function problem •Password always cannot successful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#start this function is showing User what you want ? log in or create account
 import hashlib
 
-
+#start this function is showing User what you want ? log in or create account
 def start():
     with open('start.txt','r') as file2:
         lines = file2.readlines()
@@ -28,7 +27,7 @@ def veracity(acid,pw):
                 if pw == b:
                    return True
             readver = ver.readline()
-        print("Wrong ac or pw")
+        print("Wrong accountid or password")
         return False
 
 
@@ -36,7 +35,7 @@ def signin():
     ver = False
     while ver is False:
         acid = input("ID: ")
-        pw = input("PW: ")
+        pw = hashlib.sha512(input("PW: ").encode(encoding="utf-8")).hexdigest()
         if veracity(acid,pw) is True:
             ver = True
     print ("LogIn successful")
@@ -61,27 +60,21 @@ def menu():
 
 #This function is for create account.
 def createaccount():
-    created=False
-    while created is False:
-        acid = input("Enter Account Number: ")
-        acpw = hashlib.sha512(input("Enter Password: ").encode(encoding="utf-8")).hexdigest()
-        confirm = hashlib.sha512(input("Confirm Password: ").encode(encoding="utf-8")).hexdigest()
-        while confirm != acpw:
-            print("Repeated Account Id or Wrong Password")
-            acid=input("Enter Account Number: ")
-            acpw=hashlib.sha512(input("Enter Account Password: ").encode(encoding="utf-8")).hexdigest()
-            confirm=hashlib.sha512(input("Confirm Password: ").encode(encoding="utf-8")).hexdigest()
-            with open('veracity.txt','r') as find:
-                sameid=find.readline()
-                while sameid is not None:
-                    if sameid == acid:
-                        print("Account already exists")
-                        break
-                    sameid=find.readline()
-                created=True
-    with open('veracity.txt','a+') as addaccount:
-        addaccount.write(f"{acid} {acpw}\n")
-        return
+    acid=input("enter Account ID number: ")
+    with open("veracity.txt",'r') as searching:
+        for line in searching:
+            if line.split()[0]==acid:
+                print("Account ID number has been in database")
+                return None
+        acpw = hashlib.sha512(input("enter Account Password: ").encode(encoding="utf-8")).hexdigest()
+        confrim = hashlib.sha512(input("enter Confirm Password: ").encode(encoding="utf-8")).hexdigest()
+        while acpw!=confrim:
+            acpw = hashlib.sha512(input("Please enter  again Account Password: ").encode(encoding="utf-8")).hexdigest()
+            confrim = hashlib.sha512(input("enter Confirm Password: ").encode(encoding="utf-8")).hexdigest()
+    with open("veracity.txt",'a') as adding:
+        adding.write(f"{acid} {acpw}\n")
+        print("Account Created")
+        return None
 
 #This part is the booking system how to run
 systeminform="running"
@@ -100,8 +93,5 @@ while systeminform !="exit":
                 useroption=menu()
     elif userstartingoption == 2:
         createaccount()
-        print("Account Created")
     elif userstartingoption ==3:
         break
-
-
--- test.py
+++ test.py
@@ -1,24 +1,35 @@
-def veracity(acid,pw):
-    with open('veracity.txt', 'r') as ver:
-        readver = ver.readline()
-        while readver !="":
-            a,b = readver.split()
-            if acid == a:
-                if pw == b:
-                   return True
-            readver = ver.readline()
-        print("Wrong ac or pw")
-        return False
+import hashlib
+
+def hashlibsha512(a):
+    o=hashlib.sha512(a.encode(encoding="utf-8")).hexdigest()
+    return o
+
+def isdifferent(search):
+    with open("veracity.txt",'r') as searching:
+        searchingitem=searching.readline().split()
+        while searchingitem is not None:
+            if hashlibsha512(search)==searchingitem[0]:
+                return False
+            searchingitem=searching.readline().split()
+        return True
+
+def createaccount():
+    acid=input("Enter your Id: ")
+    while acid is None or isdifferent(acid) is False:
+        print("Your acid is empty or has been used ")
+        acid=input("Enter your Id: ")
+    acpw=input("Enter your password: ")
+    confirm=input("Enter your password again: ")
+    while acpw!=confirm:
+        print("Your password is not same.")
+        acpw=input("Enter your password: ")
+        confirm=input("Enter your password again: ")
+    with open("veracity.txt",'a') as adddata:
+        acid=hashlibsha512(acid)
+        acpw=hashlibsha512(acpw)
+        adddata.write(f'{acid} {acpw}')
+    return
 
 
-def signin():
-    ver = False
-    while ver is False:
-        acid = input("ID: ")
-        pw = input("PW: ")
-        if veracity(acid,pw) is True:
-            ver = True
-    print ("LogIn successful")
 
-
-signin()
+createaccount()
